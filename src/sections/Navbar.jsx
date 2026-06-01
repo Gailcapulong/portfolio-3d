c93f6cdfb61e9c360de49482980a5d77872d9c30
Gailcapulong <gailcapulong77@gmail.com>
2026-06-01 22:54:32 +0800

    feat: add react-scroll for smooth scrolling and enhance Navbar with GSAP animations

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,5 +1,8 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { socials } from "../constants/index.js";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { Link } from "react-scroll";
 
 const Navbar = () => {
   const navRef = useRef(null);
@@ -7,6 +10,83 @@ const Navbar = () => {
   const contactRef = useRef(null);
   const topLineRef = useRef(null);
   const buttomLineRef = useRef(null);
+  const tl = useRef(null);
+  const iconTl = useRef(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const [showBurger, setShowBurger] = useState(true);
+
+  useGSAP(() => {
+    gsap.set(navRef.current, { xPercent: 100 });
+    gsap.set([linksRef.current, contactRef.current], { autoAlpha: 0, x: -20 });
+
+    tl.current = gsap
+      .timeline({ paused: true })
+      .to(navRef.current, { xPercent: 0, duration: 1, ease: "power3.out" })
+      .to(
+        linksRef.current,
+        {
+          autoAlpha: 1,
+          x: 0,
+          stagger: 0.1,
+          duration: 0.5,
+          ease: "power2.out",
+        },
+        "<",
+      )
+      .to(
+        contactRef.current,
+        {
+          autoAlpha: 1,
+          x: 0,
+          duration: 0.5,
+          ease: "power2.out",
+        },
+        "<+0.2",
+      );
+
+    iconTl.current = gsap
+      .timeline({ paused: true })
+      .to(topLineRef.current, {
+        rotation: 45,
+        y: 3.3,
+        duration: 0.3,
+        ease: "power2.inOut",
+      })
+      .to(
+        buttomLineRef.current,
+        {
+          rotate: -45,
+          y: -3.3,
+          duration: 0.3,
+          ease: "power2.inOut",
+        },
+        "<",
+      );
+  }, []);
+
+  useEffect(() => {
+    let lastScrollY = window.scrollY;
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+
+      setShowBurger(currentScrollY <= lastScrollY || currentScrollY < 10);
+
+      lastScrollY = currentScrollY;
+    };
+    window.addEventListener("scroll", handleScroll, { passive: true });
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const toggleMenu = () => {
+    if (isOpen) {
+      tl.current.reverse();
+      iconTl.current.reverse();
+    } else {
+      tl.current.play();
+      iconTl.current.play();
+    }
+    setIsOpen(!isOpen);
+  };
   return (
     <>
       <nav
@@ -17,9 +97,15 @@ const Navbar = () => {
           {["home", "services", "about", "work", "contact"].map(
             (section, index) => (
               <div key={index} ref={(el) => (linksRef.current[index] = el)}>
-                <a className="transitional-all duration-300 cursor-pointer hover:text-white">
+                <Link
+                  className="transitional-all duration-300 cursor-pointer hover:text-white"
+                  to={`${section}`}
+                  smooth
+                  offset={0}
+                  duration={1000}
+                >
                   {section}
-                </a>
+                </Link>
               </div>
             ),
           )}
@@ -52,7 +138,18 @@ const Navbar = () => {
           </div>
         </div>
       </nav>
-      <div className="fixed z-50 flex flex-col items-center justify-center gap-1 transition-all duration-300 bg-black rounded-full cursor-pointer w-14 h-14 md:w-20 md:h-20 top-4 right-10">
+      <div
+        className="fixed z-50 flex flex-col items-center justify-center gap-1 transition-all duration-300 bg-black rounded-full cursor-pointer w-14 h-14 
+      md:w-20 md:h-20 top-4 right-10"
+        onClick={toggleMenu}
+        style={
+          showBurger
+            ? { clipPath: " circle(51.1% at 50% 50%)" }
+            : {
+                clipPath: " circle(0% at 50% 50%)",
+              }
+        }
+      >
         <span
           ref={topLineRef}
           className="block w-8 h-0.5 bg-white rounded-full origin-center"
