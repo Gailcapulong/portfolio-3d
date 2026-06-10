4ae7ca85d3b121b6d075cd5f5cf314464383e30d
Gailcapulong <gailcapulong77@gmail.com>
2026-06-10 22:33:38 +0800

    feat: add ContactSummary section and enhance Works section with GSAP animations and mouse interactions

---
+++ src/sections/ContactSummary.jsx
@@ -0,0 +1,8 @@
+import { useRef } from "react";
+
+const ContactSummary = () => {
+  const containerRef = useRef;
+  return <section ref={containerRef}></section>;
+};
+
+export default ContactSummary;
