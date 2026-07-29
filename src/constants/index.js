1bf7766b8cc5d6f970ea14531aa2ed3502c4d3da
Gailcapulong <gailcapulong77@gmail.com>
2026-07-29 22:25:30 +0800

    feat: update project frameworks in constants; replace React with Shadcn and Tailwind CSS with Imagekit.io

--- src/constants/index.js
+++ src/constants/index.js
@@ -87,11 +87,11 @@ export const projects = [
     image: "/assets/projects/NW.png",
     bgImage: "/assets/backgrounds/blanket.jpg",
     frameworks: [
-      { id: 1, name: "React" },
-      { id: 2, name: "Next.js" },
-      { id: 3, name: "Node.js" },
+      { id: 1, name: "Next.js" },
+      { id: 2, name: "Node.js" },
+      { id: 3, name: "Shadcn" },
       { id: 4, name: "MongoDB" },
-      { id: 5, name: "Tailwind CSS" },
+      { id: 5, name: "Imagekit.io" },
     ],
   },
   {
