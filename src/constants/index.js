94c4e8edce73166b1d2a6b925c827aac77ad4925
Gailcapulong <gailcapulong77@gmail.com>
2026-08-26 21:28:48 +0800

    fix: update wording in Rank Lens project description for clarity

--- src/constants/index.js
+++ src/constants/index.js
@@ -129,7 +129,7 @@ export const projects = [
     id: 4,
     name: "Rank Lens - ( in development )",
     description:
-      "A multi-category featuring electronics, appliances, and gaming gears with special offer.",
+      "A multi-category featuring electronics, appliances, and gaming gears.",
     href: "https://github.com/Gailcapulong/RankLens-SEO-tracker",
     image: "/assets/projects/RKL.png",
     bgImage: "/assets/backgrounds/poster.jpg",
