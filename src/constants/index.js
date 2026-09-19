394bf61f2d3b9d233cadbe3e31a56a41a751d85a
Gailcapulong <gailcapulong77@gmail.com>
2026-09-19 22:30:01 +0800

    fix: correct typo in iMessage project description

--- src/constants/index.js
+++ src/constants/index.js
@@ -98,7 +98,7 @@ export const projects = [
     id: 2,
     name: "iMessage ",
     description:
-      "A responsive web-base messaging application that enables real-time communication through a clean and user-friendly interface.",
+      "A responsive web-base message application that enables real-time communication through a clean and user-friendly interface.",
     href: "https://imessage-wv91.onrender.com",
     image: "/assets/projects/IMssg.png",
     bgImage: "/assets/backgrounds/curtains.jpg",
