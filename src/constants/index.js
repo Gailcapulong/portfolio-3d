ed139ff01bc07366b393e14fe8d8f301a406f22a
Gailcapulong <gailcapulong77@gmail.com>
2026-09-20 21:10:27 +0800

    fix: correct typo in description of The Movie App project

--- src/constants/index.js
+++ src/constants/index.js
@@ -145,7 +145,7 @@ export const projects = [
     id: 5,
     name: "The Movie App",
     description:
-      "A curate collection of designer home decor items, including furnitures and vases.",
+      "A curate collection of designer home decor items, furnitures and vases.",
     href: "https://movie-app-silk-iota.vercel.app/",
     image: "/assets/projects/theMvapp.png",
     bgImage: "/assets/backgrounds/table.jpg",
